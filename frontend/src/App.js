61485751f4009a1fbc9b8692aa37fb569f94c48e
vallarasu <vallarasusaravanakumar@gmail.com>
2026-03-24 17:34:58 +0530

    initial commit - portfolio

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css'
+import Hero from "./components/hero";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
